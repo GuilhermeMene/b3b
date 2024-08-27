b6e4b05888a1079648831979ff5e8bf83325f453
Guilherme Mene <guilherme.ale.primo@live.com>
2024-08-27 17:48:52 +0100

    The first tested version of the B3B

---
+++ b3b/logger.py
@@ -0,0 +1,51 @@
+"""
+Logging all errors and results from backtesting 
+"""
+
+import os 
+
+def set_path(path):
+    """
+    Method for set the log path in system environment
+    """
+    try:
+        os.environ['LOGPATH'] = path
+    except Exception as e:
+        print('An error occurred. The log path cannot be defined.')
+
+def log(logtext:str):
+    """
+    Method for save the log in text file 
+    """
+    try:
+        filedir = os.environ.get('LOGPATH')
+        filepath = os.path.join(filedir, 'Log.txt')
+
+        with open(filepath, 'a') as logfile:
+            logfile.write(logtext + '\n')
+            logfile.close()
+
+    except Exception as e:
+        print('An error occurred.The log path is inaccessible \
+              or the the log path not in os environment.')
+
+def save_trade(order, ticker:str ,pricetype='High'):
+    """
+    Method for save the trades from backtesting
+    """
+
+    try:
+        header = 'Datetime,Price,Quantity,Total,Type'
+        path = f'Trades_{ticker}_{pricetype}.csv'
+
+        if not os.path.exists(path):
+            with open(path, 'w') as tfile:
+                tfile.write(header + '\n')
+                tfile.write(str(order) + '\n')
+                tfile.close()
+        else:
+            with open(path, 'a') as tfile:
+                tfile.write(str(order) + '\n')
+                tfile.close()
+    except Exception as e:
+        print('An error occurred. The trades files is not acessible, and the trade result cannot be saved.')
---
+++ b3b/strategy.py
@@ -0,0 +1,41 @@
+"""
+Method for calculate the strategy using the indicators 
+"""
+
+import pandas as pd
+import pandas_ta as ta
+from main import logger
+
+
+def get_entry_exit(data:pd.DataFrame):
+    """
+    Method for calculate and populate the dataframe with entry and exit points
+    """
+
+    try:
+        #Calculating the indicators
+        bb_len = 14
+        bb = ta.bbands(data['Close'], length=14)
+        data['rsi'] = ta.rsi(data['Close'], length=5)
+
+        #Calculating the entry point
+        data.loc[
+            (
+                (bb[f'BBL_{bb_len}_2.0'] > data['Low']) & 
+                (data['rsi'] < 30)
+        ),
+        'entry'] = 1
+
+        #Calculating the exit point
+        data.loc[
+            (
+                (bb[f'BBU_{bb_len}_2.0'] < data['High']) &
+                (data['rsi'] > 70)
+        ),
+        'exit'] = 1
+
+        return data
+
+    except Exception as e:
+        logger.log(f'An error occurred calculating and populating the entry and exit point. \
+                    Error: {e}')
